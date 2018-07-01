4a49a60a1efd2ae33b4fe24d22d8520329084174
Jim Beck <jamesbeckjr@gmail.com>
2018-06-30 21:36:45 -0400

    Updated PlayerMovementController.cs
    Player now moves with the arrow keys

--- 2D-Platformer/Assets/PlayerMovementController.cs
+++ 2D-Platformer/Assets/PlayerMovementController.cs
@@ -2,5 +2,17 @@
 
 public class PlayerMovementController : MonoBehaviour
 {
+    [SerializeField]
+    private int playerMovementSpeed = 10;
+    private void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
 
+        Vector3 movement = new Vector3(horizontal, vertical);
+
+        Debug.Log("Movement = " + movement);
+
+        transform.position += movement * playerMovementSpeed * Time.deltaTime;
+    }
 }
